40c9ce4d59ff344f0c55588071c2622f2d2b20a9
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-19 20:40:15 +0300

    feat: project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# MERN-GraphQL
+# mern-stack
+
+.env
+MONGODB_URI=
+PORT=
+JWT_SECRET_KEY=
---
+++ schema/schema.js
@@ -0,0 +1,40 @@
+const Book = require("../models/BookModel");
+
+const { 
+    GraphQLSchema, 
+    GraphQLObjectType, 
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList,
+} = require('graphql');
+
+const BookType = new GraphQLObjectType({
+    name: "Book",
+    fields: () => ({
+        title: { type: GraphQLString },
+        subtitle: { type: GraphQLString },
+        author: { type: GraphQLString },
+        description: { type: GraphQLString },
+        image: { type: GraphQLString },
+        category: { type: GraphQLString },
+        pages: { type: GraphQLInt },
+        published: { type: GraphQLString },
+    })
+})
+
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return Book.find({});
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery, 
+});
